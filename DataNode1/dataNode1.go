010886b8ff8c9b0403e25381af62982994852bdc
Sebastian <sebastian.calderon@sansano.usm.cl>
2020-11-22 17:03:11 -0300

    Se agregaron las carpetas

---
+++ DataNode1/dataNode1.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "google.golang.org/Tarea2SD/Client/Servicio"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":50051"
+)
+
+// server is used to implement helloworld.GreeterServer.
+type server struct {
+	pb.UnimplementedEstructuraCentralizadaServer
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) Hello(ctx context.Context, in *pb.T) (*pb.T, error) {
+	log.Printf("LLego un mensaje")
+	return &pb.T{Mensaje: "Hello"}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterEstructuraCentralizadaServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
